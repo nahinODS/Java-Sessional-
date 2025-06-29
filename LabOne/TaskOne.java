4c5bf126ba97c6d8ce52529bfd8af91b3d214ab6
nahinODS <abdullahalnahin23@gmail.com>
2025-06-29 12:31:26 +0600

    Create TaskOne.java

---
+++ LabOne/TaskOne.java
@@ -0,0 +1,11 @@
+class Main
+{
+public static void main(String[] args) {
+String name = "Md Abdulah Al Nahin";
+String studentId = "E243015";
+String email = "abdullahalnahin23@gmail.com";
+String number = "01894178202";
+String address = "Mirsarai,, Chattogram ";
+System.out.println("Name: " + name +"\nStudent Id: "+ studentId + "\nEmail: "+email +"\nPhone: "+ number +"\nAddress: "+ address);
+}
+}
