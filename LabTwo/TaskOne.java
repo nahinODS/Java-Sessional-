c4c7491e3f04ccd123b13aeca03b299cf9d06ee2
nahinODS <abdullahalnahin23@gmail.com>
2025-06-29 12:41:52 +0600

    Update TaskOne.java

--- LabTwo/TaskOne.java
+++ LabTwo/TaskOne.java
@@ -1,15 +1,31 @@
-import java.util.Scanner ;
+
+import java.util.Scanner;
 class Main{
-public static void main(String[] args) {
-Scanner cal = new Scanner(System.in);
-System.out.print("Enter first digit: ");
-int num1 = cal.nextInt();
-System.out.print("Enter second digit: ");
-int num2 = cal.nextInt();
-System.out.println("Addition: "+ (num1+num2));
-System.out.println("Subtraction: "+ (num1-num2));
-System.out.println("Multiplication: "+ (num1*num2));
-System.out.println("Division: "+ (num1/num2));
-cal.close();
-}
+    public static void main(String[] args) {
+        Scanner grade = new Scanner(System.in);
+        System.out.print("Enter your mark: ");
+        int mark = grade.nextInt();
+        if (mark >=80) {
+            System.out.printf("Your grade is A+");
+        }
+        else if(mark>=70){
+            System.out.printf("Your grade is A ");
+        }
+        else if(mark>=60){
+            System.out.printf("Your grade is A-");
+        }
+        else if(mark>=50){
+            System.out.printf("Your grade is B");
+        }
+        else if(mark>=40){
+            System.out.printf("Your grade is C");
+        }
+        else if(mark>=33){
+            System.out.printf("Your grade is D");
+        }
+        else{
+            System.out.printf("Your grade is F");
+        }
+        grade.close();
+    }
 }
